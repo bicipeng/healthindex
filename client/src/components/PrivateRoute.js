d6b0a470771aa46cbeddd4b83b4134afa2b21489
bicipeng <40676586+bicipeng@users.noreply.github.com>
2021-02-17 09:12:47 -0800

    Update PrivateRoute.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux"
 import { Redirect, Route } from "react-router-dom"
-//private route referencr 
-//https://medium.com/@thanhbinh.tran93/private-route-public-route-and-restricted-route-with-react-router-d50b27c15f5e
+
 const PrivateRoute = ({ component: Component, auth: { isAuth, loading }, ...rest }) => 
 (<Route {...rest} render={props => !isAuth && !loading ?
      (<Redirect to="/login" />) : (<Component {...props} />)} />)
